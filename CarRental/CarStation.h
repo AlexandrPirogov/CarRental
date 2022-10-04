d3d0719f2624df1682107905db75c04b4d7f13a5
AlexandrPirogov <cat_shot12@mail.ru>
2022-10-04 21:47:19 +0300

    added check for dupliactes

--- CarRental/CarStation.h
+++ CarRental/CarStation.h
@@ -32,14 +32,26 @@ typedef std::map<int, std::unique_ptr<Car>> carRecord;
 
 class CarStation
 {
+	short add_status;
 	stationCars cars;
 public:
-	CarStation() = default;
+	const short ADD_STATUS_NIL = -1;
+	const short ADD_STATUS_OK = 0;
+	const short ADD_STATUS_DUPLICATE = 1;
+
+	CarStation() : add_status(ADD_STATUS_NIL) { add_status = ADD_STATUS_NIL; };
 
 	inline void addCar(std::unique_ptr<Car>& car)
 	{
 		std::string title = car->carTitle();
+		if (cars[title].find(car->carNumber()) == cars[title].end())
+		{
+			add_status = ADD_STATUS_DUPLICATE;
+			return;
+		}
+
 		cars[title].emplace(std::make_pair(car->carNumber(), std::move(car)));
+		add_status = ADD_STATUS_OK;
 	}
 
 	inline int avaibleCars() noexcept
@@ -61,5 +73,10 @@ public:
 	{
 		return false;
 	};
+
+	inline const short addStatus() noexcept
+	{
+		return add_status;
+	}
 };
 
